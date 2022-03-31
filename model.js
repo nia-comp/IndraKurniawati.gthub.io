e3f69aa65870f9b4ec665ebf20ff6c8676c83477
nia-comp <76938533+nia-comp@users.noreply.github.com>
2022-03-31 23:20:26 +0700

    Update model.js

--- model.js
+++ model.js
@@ -1,80 +1,81 @@
 var screen=document.querySelector('#screen');
-var btn=document.querySelectorAll('.btn');
+    var btn=document.querySelectorAll('.btn');
 
-for(item of btn)
-{
-    item.addEventListener('click',(e)=>{
-        btntext=e.target.innerText;
-
-        if(btntext =='×')
-        {
-            btntext= '*';
-        }
-
-        if(btntext=='÷')
-        {
-            btntext='/';
-        }
-        screen.value+=btntext;
-    });
-}
-
-function sin()
-{
-    screen.value=Math.sin(screen.value);
-}
-
-function cos()
-{
-    screen.value=Math.cos(screen.value);
-}
-
-function tan()
-{
-    screen.value=Math.tan(screen.value);
-}
-
-function pow()
-{
-    screen.value=Math.pow(screen.value,2);
-}
-
-function sqrt()
-{
-    screen.value=Math.sqrt(screen.value,2);
-}
-
-function log()
-{
-    screen.value=Math.log(screen.value);
-}
-
-function pi()
-{
-    screen.value= 3.14159265359;
-}
-
-function e()
-{
-    screen.value=2.71828182846;
-}
-
-function fact()
-{
-    var i, num, f;
-    f=1
-    num=screen.value;
-    for(i=1; i<=num; i++)
+    /*============ For getting the value of btn, Here we use for loop ============*/
+    for(item of btn)
     {
-        f=f*i;
+        item.addEventListener('click',(e)=>{
+            btntext=e.target.innerText;
+
+            if(btntext =='×')
+            {
+                btntext= '*';
+            }
+
+            if(btntext=='÷')
+            {
+                btntext='/';
+            }
+            screen.value+=btntext;
+        });
     }
 
-    i=i-1;
+    function sin()
+    {
+        screen.value=Math.sin(screen.value);
+    }
 
-    screen.value=f;
-}
+    function cos()
+    {
+        screen.value=Math.cos(screen.value);
+    }
 
-function backspc()
-{
-    screen.value=screen.value.substr(0,screen.value.length-1);
-}
+    function tan()
+    {
+        screen.value=Math.tan(screen.value);
+    }
+
+    function pow()
+    {
+        screen.value=Math.pow(screen.value,2);
+    }
+
+    function sqrt()
+    {
+        screen.value=Math.sqrt(screen.value,2);
+    }
+
+    function log()
+    {
+        screen.value=Math.log(screen.value);
+    }
+
+    function pi()
+    {
+        screen.value= 3.14159265359;
+    }
+
+    function e()
+    {
+        screen.value=2.71828182846;
+    }
+
+    function fact()
+    {
+        var i, num, f;
+        f=1
+        num=screen.value;
+        for(i=1; i<=num; i++)
+        {
+            f=f*i;
+        }
+
+        i=i-1;
+
+        screen.value=f;
+    }
+
+    function backspc()
+    {
+        screen.value=screen.value.substr(0,screen.value.length-1);
+    }
